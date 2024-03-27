40de24b766ca63c9c2080b92d473a0a0239934a5
ramyakali <ramyacse1295@gmail.com>
2024-03-27 09:20:02 +0530

    Initial Commit

---
+++ controller.js
@@ -0,0 +1,45 @@
+
+
+var loginUrl = "https://retoolapi.dev/wOZG9C/InvLogin";
+var productsUrl = "https://retoolapi.dev/qf4LYd/InvProducts";
+var cartUrl = "https://retoolapi.dev/3KELBn/InvCart";
+
+function registerEmployee(){
+
+}
+
+function employeeLogin(){
+
+}
+
+function displayProducts(){
+
+}
+
+function addToCart(){
+
+}
+
+function displayCart(){
+
+}
+
+function approveProduct(){
+
+}
+
+function rejectProduct(){
+
+}
+
+function displayEmployees(){
+
+}
+
+function approveEmployee(){
+
+}
+
+function rejectEmployee(){
+
+}
